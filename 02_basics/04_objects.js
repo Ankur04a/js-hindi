5dfc843eebddda96ebdf1e14af9ef0d9c6631bc7
Ankur04a <129254236+Ankur04a@users.noreply.github.com>
2024-04-03 16:31:13 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,68 @@
+//SINGALTON OBJECTS
+
+//const tinderUser=new Object(); //way to declare object
+
+const tinderUser={}      //another way to declare object
+tinderUser.id="abc123";
+tinderUser.name="Sammy";
+tinderUser.isLoggedIn=false;
+
+console.log(tinderUser); 
+
+//object nesting
+
+const regularUser={
+    email:"ankurtang1111@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname:"Ankur",
+            lastname:"kumar",
+        }
+    }
+
+}
+console.log(regularUser.fullname.userfullname.firstname);
+
+//merge objects
+
+const obj1={1:"a" , 2:"b" }
+const obj2={3:"a" , 4:"b" } 
+
+//const obj3=obj1 + obj2 =>give problem aaisa hoga ki object merge hoga uska element nhi
+
+//best approch to merge objects
+const obj3=Object.assign({},obj1,obj2); //empty dena chahiye =>nhi denge fir bhi chalega
+console.log(obj3);
+
+//another way to merge and best way =>spread onjects like 
+
+const obj4={...obj1,...obj2};
+console.log(obj4);
+
+//database se jab object aata hai toh wo array of objects aata hai =>
+const user=[
+    {
+        id:"ab01",
+        email:"ak@gmail.com"
+    },
+    {
+        id:"ab01",
+        email:"ak@gmail.com"
+    },
+    {
+        id:"ab01",
+        email:"ak@gmail.com"
+    }
+]
+console.log(user[1].email);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser)); //IMP=>isme jo key dega uska datatype array hoga
+console.log(Object.values(tinderUser)); //value dega saare keys ke
+console.log(Object.entries(tinderUser)); //alag alag kr dega key and values ke pair ko
+
+
+
+//agar pata krna hai ki object mai koi property hai ki nhi toh kaise pta karenge =>
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
