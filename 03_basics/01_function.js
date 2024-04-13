8b6ddccb17a399c66391f36893e09d5c0e3d3441
Ankur04a <129254236+Ankur04a@users.noreply.github.com>
2024-04-13 20:17:46 +0000

    function basics

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -34,6 +34,35 @@ function userLoggedIn(username){
 //console.log(userLoggedIn("Ankur"))
 console.log(userLoggedIn()) //=>output==>undefined then just logged in
 
+//rest operator
+function calculateCartPrice(value1,value2,...num1){ //...used as rest operator isme kya hota hai jaise ek num1 hhai lekin agar bahut value millega toh wo ussko array mai store kr lega
+    return num1;
+}
+console.log(calculateCartPrice(200,300,400,1000,10));//output=>400,1000,10 ? 200,300 mhi hoga q ki wo value 1 and value2 mai challa gaya hai aur hum log num1 retun kiye hai
+
+
+const user={
+    username:"Ankur",
+    price:99
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user);
+
+//object direct bhi pass kr sakte hai
+handleObject({
+    username:"Guppi",
+    price:56
+})
+
+//function and array
+
+const newArray=[100,200,300,400,500]
+function secondArray(getArray){
+    return getArray[2];
+}
+console.log(secondArray(newArray))
 
 
 
